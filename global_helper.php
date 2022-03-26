ba5be228ca757d22d5bfb6af9ea86bd743a3a22f
dhonstudio <doon13@gmail.com>
2022-03-27 03:31:15 +0700

    Adding indonesian date function

--- global_helper.php
+++ global_helper.php
@@ -16,3 +16,30 @@ function get_word_between(string $string, string $start, string $end){
     $len = strpos($string, $end, $ini) - $ini;
     return substr($string, $ini, $len);
 }
+
+/**
+ * Get month in Bahasa
+ *
+ * @param	string	$fullDate
+ * @return	string
+ */
+function indonesian_date(string $fullDate){
+    $month = array (
+	    1 => 'Januari',
+	    'Februari',
+	    'Maret',
+	    'April',
+	    'Mei',
+	    'Juni',
+	    'Juli',
+	    'Agustus',
+	    'September',
+	    'Oktober',
+	    'November',
+	    'Desember'
+    );
+    
+    $date = explode('-', $fullDate);
+     
+    return $date[2] . ' ' . $month[ (int)$date[1] ] . ' ' . $date[0];
+}
