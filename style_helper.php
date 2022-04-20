c5a79416001a02d8ef4479caf5e3f940ce59f626
dhonstudio <doon13@gmail.com>
2022-04-20 08:20:28 +0700

    Adding Montserrat font

--- style_helper.php
+++ style_helper.php
@@ -19,11 +19,12 @@ $ci->css = [
 
 $ci->font = [
     'google-ElsieSwashCaps' => '<link href="https://fonts.googleapis.com/css?family=Elsie+Swash+Caps:400,900" rel="stylesheet">',
-    'google-Lato'           => '<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">',
+    'google-Lato'           => '<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">',
+    'google-Montserrat'     => '<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">',
     'google-Nunito'         => '<link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">',
     'google-OpenSans'       => '<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">',
     'google-Poppins'        => '<link href="https://fonts.googleapis.com/css?family=Poppins:400,700&display=swap" rel="stylesheet">',
-    'google-Raleway'        => '<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">',
+    'google-Raleway'        => '<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">',
     'google-SourceSansPro'  => '<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:200,200i,300,300i,400,400i,600,600i,700,700i,900,900i" rel="stylesheet">',
 ];
 
