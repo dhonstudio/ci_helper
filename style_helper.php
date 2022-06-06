b45ce50def7c56ddf8edbb72def904d6de75d5c2
dhonstudio <doon13@gmail.com>
2022-06-06 14:32:07 +0700

    fixed style_helper

--- style_helper.php
+++ style_helper.php
@@ -3,8 +3,7 @@
 $ci = get_instance();
 
 $ci->css = [
-    'bootstrap461'      => '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">',
-    'bootstrap513'      => '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">',
+    'bootstrap513min'       => '<link href="https://dhonstudio.com/assets/vendor/bootstrap-5.1.3-dist/css/bootstrap.min.css" rel="stylesheet">',
 ];
 
 $ci->font = [
@@ -18,4 +17,7 @@ $ci->font = [
     'google-SourceSansPro'  => '<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:200,200i,300,300i,400,400i,600,600i,700,700i,900,900i" rel="stylesheet">',
 ];
 
-$ci->js = [];
+$ci->js = [
+    'bootstrapbundle513min' => '<script src="https://dhonstudio.com/assets/vendor/bootstrap-5.1.3-dist/js/bootstrap.bundle.min.js"></script>',
+    'jquery360'             => '<script src="https://dhonstudio.com/assets/vendor/jquery/jquery-3.6.0.min.js"></script>',
+];
